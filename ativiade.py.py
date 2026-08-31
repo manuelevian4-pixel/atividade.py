e758a29727892c8ddce6fd720633f81bd134f075
Manu Viana <manuelevian4@gmail.com>
2026-08-30 22:03:52 -0300

    Add files via upload

---
+++ ativiade.py.py
@@ -0,0 +1,11 @@
+opcao = int(input("Escolha (1-3):"))
+
+match opcao:
+ case 1:
+    print("Somando...")
+ case 2:
+    print("Subtraindo...")  
+ case 3:
+    print("Multiplicando...")
+ case 4:
+    print("Opção inválida")
